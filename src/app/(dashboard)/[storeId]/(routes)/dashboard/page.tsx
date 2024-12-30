7663903c0239573d0c4521088b6857cecc34f43a
allan <allanrey.macasaquit@gmail.com>
2024-12-29 23:19:46 -0500

    14. Added Basic Data Table

--- src/app/(dashboard)/[storeId]/(routes)/dashboard/page.tsx
+++ src/app/(dashboard)/[storeId]/(routes)/dashboard/page.tsx
@@ -2,16 +2,24 @@ import Dashboard from '@/app/(dashboard)/components/dashboard';
 import { db } from '@/app/db';
 import { dashboard } from '@/app/schema';
 import { eq } from 'drizzle-orm';
+import { DashboardColumn } from './[dashboardId]/columns';
+import { format } from 'date-fns';
 
 const DashboardPage = async ({ params }: { params: { storeId: string } }) => {
 	const { storeId } = await params;
 	const data = await db.query.dashboard.findMany({
 		where: eq(dashboard.storeId, storeId),
 	});
+
+	const formattedData: DashboardColumn[] = data.map((item) => ({
+		id: item.id,
+		label: item.label,
+		createdAt: item.createdAt ? format(item.createdAt, 'MMM do, yyyy') : 'N/A',
+	}));
 	return (
 		<div>
 			<div className='flex-1 space-y-4 p-8'>
-				<Dashboard data={data} />
+				<Dashboard data={formattedData} />
 			</div>
 		</div>
 	);
--- src/app/(dashboard)/components/dashboard.tsx
+++ src/app/(dashboard)/components/dashboard.tsx
@@ -1,13 +1,19 @@
 'use client';
 
-import { dashboard } from '@/app/schema';
 import { Button } from '@/components/ui/button';
 import Heading from '@/components/ui/heading';
 import { Plus } from 'lucide-react';
 import { useParams, useRouter } from 'next/navigation';
+import {
+	columns,
+	DashboardColumn,
+} from '../[storeId]/(routes)/dashboard/[dashboardId]/columns';
+
+import { Separator } from '@/components/ui/separator';
+import DataTable from './data-table';
 
 interface Props {
-	data: (typeof dashboard.$inferSelect)[];
+	data: DashboardColumn[];
 }
 
 const Dashboard = ({ data }: Props) => {
@@ -15,19 +21,23 @@ const Dashboard = ({ data }: Props) => {
 	const params = useParams();
 
 	return (
-		<div className='flex items-center justify-between min-w-[340px] md:max-w-5xl mx-auto'>
-			<Heading
-				title={`Dashboard (${data.length})`}
-				description='Manage Dashboard for your Store'
-			/>
-			<Button
-				variant='outline'
-				size='sm'
-				onClick={() => router.push(`/${params.storeId}/dashboard/new`)}
-			>
-				<Plus className='size-3' /> Add new
-			</Button>
-		</div>
+		<>
+			<div className='flex items-center justify-between min-w-[340px] md:max-w-5xl mx-auto'>
+				<Heading
+					title={`Dashboard (${data.length})`}
+					description='Manage Dashboard for your Store'
+				/>
+				<Button
+					variant='outline'
+					size='sm'
+					onClick={() => router.push(`/${params.storeId}/dashboard/new`)}
+				>
+					<Plus className='size-3' /> Add new
+				</Button>
+			</div>
+			<Separator className='max-w-5xl mx-auto' />
+			<DataTable columns={columns} data={data} />
+		</>
 	);
 };
 export default Dashboard;
